1e42562412630b8f4113306e9639a6b9b0cbc723
hatafmuhammad0 <hatafmuhammad0@gmail.com>
2020-04-06 23:47:55 +0500

    Function learning

---
+++ Tutorial 72 Function details.py
@@ -0,0 +1,17 @@
+# def last (a):   
+#     return(a[-1])
+
+# name = input("Type your name :" )
+
+# print(last(name))
+
+#Getting odd or even number
+
+def odd_even(a):
+    if a % 2 == 0:
+        return "even number"
+    return "odd number"
+
+print(odd_even(246))
+
+
